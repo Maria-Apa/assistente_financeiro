584eabf99dd315b5bbfa2bfdd04b0c448e7c119a
Maria Aparecida Leite da Silva <maria.apa201618@gmail.com>
2026-02-17 19:28:33 -0300

    Delete src/nega_calculo.py

--- src/nega_calculo.py
+++
@@ -1,9 +0,0 @@
-def verificar_calculo(msg):
-    msg_lower = msg.lower()
-
-    palavras = ["calcule", "simule", "quanto rende", "%", "juros"]
-
-    if any(w in msg_lower for w in palavras):
-        return "Não faço simulações ou cálculos, mas posso te explicar como o conceito funciona 😊"
-
-    return None
